f83a8ccbba9afa8cf9319d4580300047e71640fc
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-10-26 01:08:56 +0100

    Update vmware.sh

--- vmware.sh
+++ vmware.sh
@@ -1,6 +1,6 @@
 sudo apt update -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y
 sudo apt install -y curl git libaio1 build-essential linux-headers-`uname -r` linux-image-`uname -r` vlan
-sudo curl -L https://www.vmware.com/go/getworkstation-linux > Downloads/vmware.bin
+sudo curl -L https://www.vmware.com/go/getworkstation-linux > vmware.bin
 sudo chmod +x vmware.bin
 sudo ./vmware.bin
 sudo git clone -b workstation-$( grep player.product.version /etc/vmware/config | sed '/.*\"\(.*\)\".*/ s//\1/g' ) https://github.com/mkubecek/vmware-host-modules.git /opt/vmware-host-modules/
